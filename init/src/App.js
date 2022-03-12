e2fdc6357d7c12fdd98d2a95fee1f694a09f4f2b
Pierre <kindler@wesecom.com>
2022-03-12 15:46:09 +0100

    Start your engines

--- init/src/App.js
+++ init/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="spinner">
+          <div className="eight">8</div>
+          <div className="vi">V</div>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
